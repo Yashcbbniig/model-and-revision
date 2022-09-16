4d8587eeb1cbec1c2cc1622820f1c620d794e6d8
Yashaswi/192112028 <112531949+Yashcbbniig@users.noreply.github.com>
2022-09-16 13:43:56 +0530

    Add files via upload

---
+++ lowercase uppercase special characters.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+void count(string str)
+{
+ int Uppercase = 0;
+ int Lowercase = 0;
+ int digit = 0;
+ int special_character = 0;
+
+ for (int i = 0; i < str.length(); i++)
+ {
+ if (str[i] >= 'A' && str[i] <= 'Z')
+ {
+ Uppercase++;
+ }
+ else if(str[i] >= 'a' && str[i] <= 'z')
+ {
+ Lowercase++;
+ }
+ else if(str[i]>= '0' && str[i]<= '9')
+ {
+ digit++;
+ }
+ else
+ {
+ special_character++;
+ }
+ }
+ cout<<"Total Upper case letters in a string are: "<<Uppercase<< endl;
+ cout<<"Total lower case letters in a string are: "<<Lowercase<< endl;
+ cout<<"Total number in a string are: "<<digit<< endl;
+ cout<<"total of special characters in a string are: "<<special_character<< endl;
+}
+int main()
+{
+ string str = "**MaytheAlmightywillBlessUs**";
+ count(str);
+ return 0;
+}
